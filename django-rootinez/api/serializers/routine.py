d01c1eae6977f09d380b279a6b5e6abc5732c35e
Nicholas Meade <nicholasjmeade@hotmail.com>
2022-06-27 14:10:09 -0400

    finished routine serializer

---
+++ django-rootinez/api/serializers/routine.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from ..models.routine import Routine
+
+class RoutineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Routine
+        fields = ('id', 'name', 'description')
