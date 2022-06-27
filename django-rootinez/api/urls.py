c8246d9e007c790d08e52584c68e0832ffd064a4
Nicholas Meade <nicholasjmeade@hotmail.com>
2022-06-27 14:21:19 -0400

    finished urls.py for api

--- django-rootinez/api/urls.py
+++ django-rootinez/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views.routine import RoutinesView, RoutineView
 
 urlpatterns = [
-    
+    path('routines/', RoutinesView.as_view(), name='index'),
+    path('routines/<int:pk>', RoutineView.as_view(), name='routine')
 ]
